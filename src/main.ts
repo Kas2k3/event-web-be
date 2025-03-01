48712d5803f8c92c25f2da5f8fa768d33e8077d8
Hà Vĩnh Phước <122790758+Kas2k3@users.noreply.github.com>
2025-03-01 19:11:12 +0700

    base config

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  const port = configService.get('PORT') || 8080;
+
+  await app.listen(port);
+}
+bootstrap();
